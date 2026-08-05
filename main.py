7d15ed5f4092b940393ec7c1cf0f7564565ddcf4
Justin Smeya <justinsmeya@gmail.com>
2026-08-05 12:17:19 -0500

    Starting over, cleaning up

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
-# Port scanner by Justin Smeya
+# jSCAN port scanner by Justin Smeya
 
-import socket
+# Imports
+import sys
 
-# IP and port to scan
-HOST = "45.33.32.156" #scanme.nmap.org
+# Colors :)
+BLUE = "\033[34m"
+GREEN = "\033[32m"
+RED = "\033[31m"
+RESET = "\033[0m"
 
-"""
-AF_INET (default) means "Address Family, Internet."
-It tells the OS socket interface that we're using standard, 32-bit IPv4 addresses + port numbers.
-It uses a pair (host, port).
-
-SOCK_STREAM (default) means a two-way byte stream socket type.
-It uses TCP, so guarantees data order and safety.
-No fixed packet/record boundaries.
-Allows both sides to send/receive data simultaneously.
-"""
-
-for p in range(1, 65535):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    result = s.connect((HOST, p))
+if len(sys.argv) == 2:
+    print(f"{GREEN}Now scanning: {sys.argv[1]}{RESET}")
+else:
+    print(f"{RED}Invalid arguments. Please provide an IP address or hostname to scan.{RESET}")
