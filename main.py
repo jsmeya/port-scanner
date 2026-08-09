08cc68367120d6482f4cfa6915b2462974bf4162
Justin Smeya <justinsmeya@gmail.com>
2026-08-08 22:24:12 -0500

    Added scanning notifier

--- main.py
+++ main.py
@@ -11,17 +11,16 @@ RED = "\033[31m"
 RESET = "\033[0m"
 
 # Function to scan the target and each respective port
-# NOTE: Add catches for network errors later
 def scan(target: str, port: int) -> bool:
     try:
         # Creates a new endpoint and returns one socket object
         # AF_INET means a communication of standard 32-bit, IPv4 addresses paired with port numbers. Supports hostnames as well.
         # SOCK_STREAM means a reliable, connected byte stream (maps to TCP)
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.settimeout(1)
+            s.settimeout(0.5)
             # Sets up a network link using the specified IP/hostname and port.
             # Then attempts to connect to that link.
-            conn = s.connect((target, port))
+            s.connect((target, port))
             return True
     except (socket.timeout, OSError):
         return False
@@ -29,6 +28,7 @@ def scan(target: str, port: int) -> bool:
 # Arguments when running
 if len(sys.argv) == 2:
     target = sys.argv[1]
+    print(f"{BLUE}Scanning {target}...{RESET}")
 
     # Sequentially run the scan() function
     for i in range(1, 65536):
