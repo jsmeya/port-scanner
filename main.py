bdd3424d9de5cf44b3422223f58a2ae74819af8c
Justin Smeya <justinsmeya@gmail.com>
2026-08-27 13:03:43 -0500

    first run of thread pooling

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 import sys
 import socket
 from concurrent.futures import ThreadPoolExecutor
+from itertools import repeat
 
 # Colors :)
 BLUE = "\033[34m"
@@ -11,30 +12,36 @@ GREEN = "\033[32m"
 RED = "\033[31m"
 RESET = "\033[0m"
 
-port_range = range(1, 65536)
-
 # Function to scan the target and each respective port
-def scan(target: str, port: int) -> bool:
+def scan(target: str, port: int) -> tuple[bool, int]:
     try:
         # AF_INET = IPv4 (supports hostnames too)
         # SOCK_STREAM = TCP
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.settimeout(0.5)
             s.connect((target, port)) # Attempts to connect to network link
-            return True
+            return True, port
     except (socket.timeout, OSError):
-        return False
+        return False, port
+
+# Variables
+port_range = range(1, 65536)
+max_workers = 100
 
 # Arguments when running
 if len(sys.argv) == 2:
     target = sys.argv[1]
     print(f"{BLUE}Scanning '{target}'...{RESET}\n")
 
-    # Sequentially run the scan() function
-    for i in port_range:
-        if scan(target, i):
-            print(f"{GREEN}Port {i} is open.{RESET}")
-        else:
-            print(f"Port {i} is closed.")
+    # Scan ports
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        results = executor.map(scan, repeat(target), port_range)
+
+    for result, port in results:
+        if result == True:
+            print(f"{GREEN}Port {port} is open.{RESET}")
+        elif result == False:
+            print(f"Port {port} is closed.")
+
 else: # Fallback for if the user does not specify any arguments
     print(f"{RED}ERROR: Not enough arguments. Please specify an IP address or hostname to scan.{RESET}")
